dbe233360547fcf0da5c2b99dd06475cffa52cf6
Jialei <jialeicui@126.com>
2024-02-08 13:27:37 +0800

    chat history compnoent

--- src/server/ui/src/app/layout.tsx
+++ src/server/ui/src/app/layout.tsx
@@ -3,15 +3,17 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
+import Header from "@/components/Header";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
+            <Header />
             {props.children}
           </ThemeProvider>
         </AppRouterCacheProvider>
---
+++ src/server/ui/src/components/Message.tsx
@@ -0,0 +1,27 @@
+import {ReactNode} from "react";
+
+export interface MessageProps {
+    message: string
+    avatar: ReactNode
+    role: string
+}
+
+export default function Message(props: MessageProps) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: props.role === 'user' ? 'row' : 'row-reverse',
+                alignItems: 'center',
+            }}
+        >
+            {props.avatar}
+            <div
+                style={{
+                    margin: '10px',
+                }}
+            >{props.message}</div>
+        </div>
+    )
+}
+
