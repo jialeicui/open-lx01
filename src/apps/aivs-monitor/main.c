d9f758c1909f413cd2af6388e0425453de45de00
Jialei <jialeicui@126.com>
2024-02-11 12:19:30 +0800

    refactor: split file monitor

--- src/apps/aivs-monitor/main.c
+++ src/apps/aivs-monitor/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include <sys/inotify.h>
 #include <sys/types.h>
-#include <string.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <stdlib.h>
 #include <curl/curl.h>
 #include "json.h"
+#include "monitor.h"
 
-CURL *curl;
-struct curl_slist *headers = NULL;
-int current_round = 0;
-int thread_processed_rount = 0;
-int play_the_original_answer = 0;
 const char *instruction_json_path = "/tmp/mico_aivs_lab/instruction.log";
 const char *default_server_url = "http://10.0.0.196:8007/message";
 const char *config_file = "/data/aivs_monitor/config.json";
@@ -22,7 +14,12 @@ const char *keywords[] = {
     "停",
     "关灯",
     "开灯",
+    "几点了",
 };
+
+CURL *curl;
+const char *final_url = NULL;
+struct curl_slist *headers = NULL;
 
 void speaker_pause() {
     // ubus call mediaplayer player_play_operation {\"action\":\"pause\"}
@@ -72,7 +69,7 @@ int speaker_playing() {
     return 0;
 }
 
-void act_tts(char *text) {
+void act_tts(const char *text) {
     printf("tts: %s\n", text);
     // ubus call mibrain text_to_speech "{\"text\":\"text\",\"save\":0}"
     // use system() to call ubus command
@@ -107,7 +104,6 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
 
     if (strcmp(action_buf, "ignore") == 0) {
         printf("ignore this answer\n");
-        play_the_original_answer = 1;
         return body_size;
     }
 
@@ -119,7 +115,6 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     }
     if (strlen(buf) == 0) {
         printf("tts is empty, use the original answer\n");
-        play_the_original_answer = 1;
         return body_size;
     }
 
@@ -152,15 +147,17 @@ int send_context_to_server(const char *url, const char *context) {
     return 0;
 }
 
-void mute_speaker_thread(void *arg) {
+void *mute_speaker_thread(void *arg) {
+    muter_t *muter = (muter_t *)arg;
     // dead loop and wait for tts playing and pause it
     for (;;) {
         if (speaker_playing()) {
             speaker_pause();
             break;
         }
-        if (current_round != thread_processed_rount) {
-            thread_processed_rount = current_round;
+        if (muter->current_round != muter->thread_processed_rount) {
+            // TODO should be atomic
+            muter->thread_processed_rount = muter->current_round;
             break;
         }
         usleep(1000 * 100);
@@ -177,10 +174,15 @@ void wait_for_message_file_ready() {
 }
 
 const char* get_server_url() {
+    if (final_url != NULL) {
+        return final_url;
+    }
+
     // read the whole config file
     FILE *fp = fopen(config_file, "r");
     if (fp == NULL) {
         printf("open config file failed\n");
+        final_url = default_server_url;
         return default_server_url;
     }
 
@@ -193,10 +195,23 @@ const char* get_server_url() {
     int rc = get_json_value(buf, path, server, 512);
     if (rc != 0) {
         printf("get server url failed\n");
+        final_url = default_server_url;
         return default_server_url;
     }
     printf("server url: %s\n", server);
+    final_url = server;
     return server;
+}
+
+void mute_cb(muter_t *muter) {
+    printf("mute speaker\n");
+    pthread_t tid;
+    pthread_create(&tid, NULL, mute_speaker_thread, muter);
+}
+
+void send_context(const char *data) {
+    const char *url = get_server_url();
+    send_context_to_server(url, data);
 }
 
 int main() {
@@ -209,108 +224,17 @@ int main() {
     }
     headers = curl_slist_append(headers, "Content-Type: application/json");
 
-    int fd = inotify_init();
-    if (fd < 0) {
-        perror("inotify_init");
-        return -1;
-    }
-
-    int wd = inotify_add_watch(fd, instruction_json_path, IN_MODIFY);
-    if (wd < 0) {
-        perror("inotify_add_watch");
-        return -1;
-    }
-
-    const char *server_url = get_server_url();
-
-    FILE *json_file = fopen(instruction_json_path, "r");
-    fseek(json_file, 0, SEEK_END);
-    long laste_offset = ftell(json_file);
-
-    char buf[1024];
-    while (1) {
-        ssize_t n = read(fd, buf, sizeof(buf));
-        if (n < 0) {
-            perror("read");
-            return -1;
-        }
-
-        int user_sound_final = 0;
-        int user_sound_filterd = 0;
-        int speaker_muted = 0;
-
-        struct inotify_event *event = (struct inotify_event *)buf;
-
-        // check if file length changed
-        fseek(json_file, 0, SEEK_END);
-        long current_offset = ftell(json_file);
-        if (current_offset == 0) {
-            printf("file is reset\n");
-            laste_offset = 0;
-            current_round += 1;
-            thread_processed_rount = current_round;
-
-            user_sound_final = 0;
-            user_sound_filterd = 0;
-            speaker_muted = 0;
-            play_the_original_answer = 0;
-
-            continue;
-        } else if (current_offset == laste_offset) {
-            printf("file length not changed\n");
-            continue;
-        }
-
-        // read file from last offset
-        fseek(json_file, laste_offset, SEEK_SET);
-        while (fgets(buf, sizeof(buf), json_file) != NULL) {
-            printf("read %s from file\n", buf);
-            // lines contains ',"name":"Speak",' means tts, ignore it
-            if (strstr(buf, ",\"name\":\"Speak\",") != NULL) {
-                continue;
-            }
-
-            // the text must contains '"is_final": true,' to make sure it's the final result
-            if (user_sound_final == 0 && compare_json_value(buf, "payload.is_final", "true") != 0) {
-                printf("not final result\n");
-                continue;
-            }
-            user_sound_final = 1;
-
-            if (user_sound_filterd == 0 && is_blocklist_keyword(buf)) {
-                printf("blocklist keyword detected\n");
-                user_sound_filterd = 1;
-                continue;
-            }
-            user_sound_filterd = 1;
-
-            if (speaker_muted == 0) {
-                speaker_muted = 1;
-                pthread_t tid;
-                int rc = pthread_create(&tid, NULL, (void *)mute_speaker_thread, NULL);
-                if (rc != 0) {
-                    printf("create thread failed\n");
-                }
-            }
-
-            if (play_the_original_answer) {
-                // try to get the original answer
-                char answer[1024 * 2] = {0};
-                int rc = get_json_value(buf, "payload.text", answer, 1024 * 2);
-                if (rc != 0) {
-                    continue;
-                }
-                if (strlen(answer) != 0) {
-                    act_tts(answer);
-                }
-                play_the_original_answer = 0;
-            }
-
-            send_context_to_server(server_url, buf);
-        }
-        laste_offset = ftell(json_file);
-    }
     curl_easy_cleanup(curl);
+
+    monitor_t monitor = {
+        .mute_cb = mute_cb,
+        .act_tts = act_tts,
+        .is_blocklist_keyword = is_blocklist_keyword,
+        .send_to_remote = send_context,
+    };
+
+    monitor_init(monitor);
+    monitor_start(instruction_json_path);
 
     return 0;
 }
