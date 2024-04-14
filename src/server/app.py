bce1333b2ce30b70b8660b36758a862990c0de8d
Jialei <jialeicui@126.com>
2024-04-14 10:02:55 +0800

    add azure speech service

--- src/server/app.py
+++ src/server/app.py
@@ -68,7 +68,7 @@ async def message(body: MicoMessage) -> MessageResponse:
     if text in ["开灯", "关灯", "停", "大点声", "小点声", "几点了"]:
         return ignore_resp
 
-    if '后提醒我' in text:
+    if "后提醒我" in text:
         return ignore_resp
 
     # TODO support multiple sessions
